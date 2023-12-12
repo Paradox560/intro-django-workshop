f1d4c0717f33e2f97fde0b24608c55a8178bb7fb
Paradox560 <65742681+Paradox560@users.noreply.github.com>
2023-12-12 00:06:47 -0500

    Made changes to the review fields of the API

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    review_text = models.CharField(max_length=100, blank=True)
+    reviewer = models.CharField(max_length=100, blank=True)
+    review_email = models.CharField(max_length=100, blank=True)
+    review_number = models.CharField(max_length=10, blank=True)
